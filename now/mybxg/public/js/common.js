1416edb7d7241b5bc2b16ed6f59e76bbff78843b
NAN0916 <707042109@qq.com>
2017-03-18 22:24:44 +0800

    模块化处理

--- now/mybxg/public/js/common.js
+++ now/mybxg/public/js/common.js
@@ -1,13 +1,54 @@
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+define(['jquery','echarts','cookie'],function($,echarts){
+    $('.navs ul').prev('a').on('click', function () {
+        $(this).next().slideToggle();
+    });
 
     var flag = $.cookie('PHPSESSID');
-
     var pathname=location.pathname;
     if(!flag && pathname.indexOf('login')==-1){
         // 没有登录
         location.href = '/login'
     }
-    
+
+//实现登陆功能
+    $("#loginForm").submit(function () {
+        var formData = $(this).serialize();
+        $.ajax({
+            type:'post',
+            url: '/api/login',
+            data:formData,
+            dataType:"json",
+            success:function (data) {
+                if(data.code==200)
+                {
+                    //转为字符串
+                    var logInfo=JSON.stringify(data.result);
+                    $.cookie('logInfo',logInfo,{path : '/'});
+                    location.href = '/index/index';
+                }
+            },
+            error:function (data) {
+                //打印错误信息
+                console.log(data.responseText);
+            }
+        })
+        return false;
+    });
+
+
+
+
+    //设置头像和名字
+    var obj=JSON.parse($.cookie('logInfo'));
+
+    $(".aside .profile img").attr("src",obj.tc_avatar);
+    $(".aside .profile h4").html(obj.tc_name);
+
+
+
+})
+
+
+
+
+
